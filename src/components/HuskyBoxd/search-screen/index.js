5c9099494ce04483255865690c96ed386562f2af
LANGemmmmm <jiedong_lang@outlook.com>
2022-04-25 20:58:24 -0400

    update navigation and search

--- src/components/HuskyBoxd/search-screen/index.js
+++ src/components/HuskyBoxd/search-screen/index.js
@@ -1,110 +1,19 @@
-import React, {useEffect, useRef, useState} from "react";
+import React from "react";
 import SearchList from '../search-list';
-import SearchItem from './search-item'
-import Navigation from "../Navigation";
-import {Link, useNavigate, useParams} from "react-router-dom";
-import axios from "axios";
+
 
 
 const SearchScreen = () => {
+    return(
+        <div>
 
-    const titleSearchRef = useRef()
-    const {movieSearch} = useParams()
-    const navigate = useNavigate()
-    const [movies, setMovies] = useState([])
-    const searchUrl = 'https://www.omdbapi.com/?apikey=436c0bf1'
-                       //'http://www.omdbapi.com/?plot=full&apikey=436c0bf1&i='
-
-    const searchByTitle = async () => {
-        const searchString = titleSearchRef.current.value || movieSearch
-        const response = await axios.get(`${searchUrl}&s=${searchString}`)
-        setMovies(response.data.Search)
-        titleSearchRef.current.value = searchString
-        navigate(`/huskyboxd/search/${searchString}`)
-    }
-
-    useEffect(() => {
-        searchByTitle()
-    }, [])
-
-
-    return (
-        <>
             <div>
-                <Navigation login={true}/>
-
-                <div className="wd-search">
-                        <input ref={titleSearchRef} type="text"/>
-                        <button className="fas fa-search" onClick={searchByTitle}/>
-                        {/*<i className="fas fa-search"/>*/}
-                </div>
+                <SearchList/>
             </div>
 
-        <div className="position-relative">
-            {/*<h1>Omdb Search</h1>*/}
-            {/*{profile.email}*/}
-            <ul className="list-group">
-                <li className="list-group-item">
-                    {/*<button*/}
-                    {/*    onClick={searchByTitle}*/}
-                    {/*    className="btn btn-primary float-end" color="red">Search</button>*/}
-                    {/*<input*/}
-                    {/*    ref={titleSearchRef}*/}
-                    {/*    className="form-control w-75"/>*/}
 
-                    {/*<div className="wd-search">*/}
-                    {/*    <form id="searchForm">*/}
-                    {/*        <input ref={titleSearchRef} type="text" name = "query"/>*/}
-                    {/*        <button className="fas fa-search" onClick={searchByTitle}/>*/}
-                    {/*        /!*<i className="fas fa-search"/>*!/*/}
-                    {/*    </form>*/}
-                    {/*</div>*/}
-
-                </li>
-                {
-                    movies.map (movie =>
-                        <SearchItem movie={movie}/>
-                    )
-                }
-            </ul>
         </div>
 
-        </>
     );
-
-    // return(
-    //     <div>
-    //         {/*<div>*/}
-    //         {/*    <Navigation login={true}/>*/}
-    //         {/*</div>*/}
-    //
-    //         <div className="wd-search">
-    //             <form id="searchForm">
-    //                 <input ref={titleSearchRef} type="text" name = "query"/>
-    //                 <button className="fas fa-search" onClick={searchByTitle}/>
-    //                 {/*<i className="fas fa-search"/>*/}
-    //             </form>
-    //         </div>
-    //         <>
-    //             {
-    //                 movies.map(movie =>
-    //                     <li className="list-group-item">
-    //                         <Link to={`/details/${movie.imdbID}`}>
-    //                             <img src={movie.Poster} className="me-2" height={30}/>
-    //                             {movie.Title}
-    //                         </Link>
-    //                     </li>
-    //                 )
-    //             }
-    //         </>
-    //
-    //         {/*<div className="position-relative">*/}
-    //         {/*    <SearchList movies = {movies}/>*/}
-    //         {/*</div>*/}
-    //
-    //
-    //     </div>
-    //
-    // );
 };
 export default SearchScreen;
--- src/components/HuskyBoxd/search-screen/search-item.js
+++ src/components/HuskyBoxd/search-screen/search-item.js
@@ -1,51 +1,34 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
-const SearchItem = ({movie})=> {
-    console.log(movie)
+const SearchItem = ({prof={
+    "_id":"123",
+    "filmname": "BatMan", "time": "2008", "alternativetitle": "黑暗骑士, Batman: The Dark Knight, 배트맨 다크 나이트, Batman, Le Chevalier Noir, 蝙蝠侠2：黑暗骑士, Темный рыцарь, 蝙蝠俠—黑夜之神",
+    "filmpicture": "../pictures/batman.jpg",     "director": "Christopher Nolan"
+}})=> {
+
     return (
         <div>
 
             <div className="container" style={{"border-style":"solid", "border-width":"thin","border-color":"white", "border-left":"none", "border-right":"none", "border-bottom":"none"}}>
                 <div className="row">
                     <div className="col-2">
-                        <Link to={`/details/${movie.imdbID}`}>
-                            < img src={movie.Poster}  height="150px" width="100%" alt={''}/>
+                        <Link to="/movie/profile">
+                        <img src={prof.filmpicture}  height="150px" width="100%" alt={''}/>
                         </Link>
                     </div>
                     <div className="col-10">
-                        <text style={{"padding-right":"20px"}}><strong>{movie.Title}</strong></text>
-                        <text style={{"padding-right":"20px"}}><strong>{movie.Year}</strong></text>
+                        <text style={{"padding-right":"20px"}}><strong>{prof.filmname}</strong></text>
+                        <text style={{"padding-right":"20px"}}><strong>{prof.time}</strong></text>
                         <br/>
-                        <text>Alternative titles: {movie.Plot}</text>
+                        <text>Alternative titles: {prof.alternativetitle}</text>
                         <br/> <br/>
-                        <text>Directed by: <strong>{movie.Director}</strong> </text>
+                        <text>Directed by: <strong>{prof.director}</strong> </text>
                     </div>
                 </div>
             </div>
 
         </div>
-        // <div className="list-group-item">
-        //
-        //     <div className="container" style={{"border-style":"solid", "border-width":"thin","border-color":"white", "border-left":"none", "border-right":"none", "border-bottom":"none"}}>
-        //         <div className="row">
-        //             <div className="col-2">
-        //                 <Link to="/movie/profile">
-        //                 <img src={prof.Poster}  height="150px" width="100%" alt={''}/>
-        //                 </Link>
-        //             </div>
-        //             <div className="col-10">
-        //                 <text style={{"padding-right":"20px"}}><strong>{prof.Ti}</strong></text>
-        //                 <text style={{"padding-right":"20px"}}><strong>{prof.time}</strong></text>
-        //                 <br/>
-        //                 <text>Alternative titles: {prof.alternativetitle}</text>
-        //                 <br/> <br/>
-        //                 <text>Directed by: <strong>{prof.director}</strong> </text>
-        //             </div>
-        //         </div>
-        //     </div>
-        //
-        // </div>
 
 
     );
